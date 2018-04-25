a1d1428ea9fd3cbb6c6bd7131f7302d952dbc0d5
BiancaSilvaLins <bb.lins.14@gmail.com>
2018-04-25 19:38:16 -0300

    Add files via upload

---
+++ Biblioteca Online/src/java/biblioteca/negocio/basica/Autor.java
@@ -0,0 +1,55 @@
+package biblioteca.negocio.basica;
+/**
+ *
+ * @author Bianca Lins
+ */
+public class Autor {
+    private Integer id;
+    private String nome;
+    private String email;
+
+    public Autor() {
+
+    }
+
+    public Autor(Integer id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Autor(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+}
+
